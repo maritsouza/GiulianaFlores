87c5915506f9086949f73567e183dced85a683c2
Maria Souza <mariatsouza02@gmail.com>
2024-10-03 22:06:40 +0100

    Inicio da atividade 07 - criando usuário, login positivo e login negativo

--- features/giuliana_flores.feature
+++ features/giuliana_flores.feature
@@ -1,16 +1,66 @@
 Feature: Site Giuliana Flores
 
-    Scenario Outline: Fazer cadastro de usuário
+    Background:
         Given que acesso o site da Giuliana Flores
-        When clico em Perfil e depois em "LOGIN / CADASTRAR"
+
+    Scenario Outline: Fazer cadastro de usuário
+        When clico em Perfil e depois em LOGIN / CADASTRAR
         Then sou direcionado para página de IDENTICAÇÃO
         When clico em Criar cadastro
         Then sou direcionado para página de MINHA CONTA
         When preencho os campos do formulário de cadastro <nome>, <cpf>, <email>, <senha>, <cep>, <numero>, <complemento>, <telefone>
-        And espero a ação do tester de validar o recaptcha por 5 segundos
+        # And espero a ação do tester de validar o recaptcha por 50 segundos
         And clico em FINALIZAR CADASTRO
-        Then sou direcionado para página de HOME
+        # Then sou direcionado para página de HOME de novo usuário
 
     Examples:
     | nome                            | cpf         | email                                     | senha      | cep      | numero | complemento | telefone    |
     | Teresinha Isabel Analu Monteiro | 81295682842 | teresinha.isabel.monteiro@metraseg.com.br | kk9y57xpBW | 05766-270 |    289 | Casa        | 11986357660 |
+
+    Scenario: Login Positivo
+        When clico em Perfil e depois em LOGIN / CADASTRAR
+        Then sou direcionado para página de IDENTICAÇÃO
+        When preencho os campos de login com email mayaanaribeiro@pozzer.net e senha cM30MTn1nL
+        And clico em continuar
+        Then sou direcionado para página de HOME
+
+    Scenario: Login Negativo
+        When clico em Perfil e depois em LOGIN / CADASTRAR
+        Then sou direcionado para página de IDENTICAÇÃO
+        When preencho os campos de login com email sueli_almeida@agaxtur.com.br e senha cM30MTn1nL
+        And clico em continuar
+        Then aparece a mensagem de erro ATENÇÃO - e-mail ou senha inválidos!
+
+    Scenario Outline: Compra a partir do banner da home
+        When clico na primeira promoção do banner 
+        Then aparece para preencher o cep
+        When preencho com o cep 05766270, clico no endereço e em aplicar
+        Then sou direcionado para página do OUTUBRO ROSA
+        When clico no produto "Dupla de Ilustre Mini Orquídea Phalaenopsis Salmão" do id "32414"
+        Then sou direcionado para página de detalhes do produto
+        And verifico se o nome do produto é "Dupla de Ilustre Mini Orquídea Phalaenopsis Salmão" e o preço é "R$ R$ 139,90"
+        And verifico a quantidade do produto antes de adicionar ao carrinho
+        When clico em ADICIONAR AO CARRINHO
+        Then verifico se sou direcionado para a interface de selecionar a data e o período de entrega
+        When seleciono a data de entrega para 31/10/2024 e o período para Comercial
+        And clico em OK
+        Then sou direcionado para o Meu Carrinho
+        And verifico se é produto "Dupla de Ilustre Mini Orquídea Phalaenopsis Salmão" se o preço é "R$ R$ 139,90"
+        And verifico se a quantidade é 1, o valor do frete é "R$ 15,90" e o valor total é "R$ 155,80"
+        When clico em continuar
+        Then sou direcionado para a página de Entrega 01
+        When preencho os campos de entrega com <nome>, <telefone>, <numero>, <tipo_endereco>, <whatsapp>
+        And clico em Não quero cartão
+        And clico em continuar
+        Then sou direcionado para a página de pagamento
+        When escolho a opção de pagamento com PIX
+        And clico em CONCLUIR COMPRA
+        Then sou direcionado para a página de confirmação de compra
+
+        Examples:
+        | nome                            | telefone    | numero | tipo_endereco | whatsapp    |
+        | Luciana Benedita Renata Lima    | 81983159224 | 864    | Residencial   | 81983159224 |
+        
+
+
+
--- features/steps/giuliana_flores.py
+++ features/steps/giuliana_flores.py
@@ -10,6 +10,7 @@ def step_impl(context):
     context.driver.implicitly_wait(10)    
     context.driver.get('https://www.giulianaflores.com.br/')
 
+# Criar Cadastro
 @when(u'clico em Perfil e depois em "LOGIN / CADASTRAR"')
 def step_impl(context):
     context.driver.find_element(By.ID, "perfil-hidden").click()
@@ -50,9 +51,146 @@ def step_impl(context):
 def step_impl(context):
     context.driver.find_element(By.ID, "ContentSite_btnCreateCustomer").click()
 
-@then(u'sou direcionado para página de HOME')
+@then(u'sou direcionado para página de HOME de novo usuário')
 def step_impl(context):
-    context.driver.get_current_url == "https://www.giulianaflores.com.br/?newcustomer=1"
+    assert context.driver.current_url == "https://www.giulianaflores.com.br/?newcustomer=1"
     
     context.driver.quit()
     
+# Login Positivo
+@when(u'clico em Perfil e depois em LOGIN / CADASTRAR')
+def step_impl(context):
+    context.driver.find_element(By.ID, "perfil-hidden").click()
+    context.driver.find_element(By.ID, "UrlLogin").click()
+    
+@when(u'preencho os campos de login com email {email} e senha {senha}')
+def step_impl(context, email, senha):
+    context.driver.find_element(By.ID, "ContentSite_txtEmail").send_keys(email)
+    context.driver.find_element(By.ID, "ContentSite_txtPassword").send_keys(senha)
+
+@when(u'clico em continuar')
+def step_impl(context):
+    context.driver.find_element(By.ID, "ContentSite_ibtContinue").click()
+    
+@then(u'sou direcionado para página de HOME')
+def step_impl(context):
+    assert context.driver.current_url == "https://www.giulianaflores.com.br/"
+    
+    context.driver.quit()
+    
+#Login Negativo
+@then(u'aparece a mensagem de erro {mensagem}')
+def step_impl(context, mensagem):
+    assert context.driver.find_element(By.ID, "ContentSite_divMessages").text == mensagem
+    
+    context.driver.quit()
+    
+
+# Fluxo de Compra
+@then(u'aparece para preencher o cep')
+def step_impl(context):
+    context.driver.find_element(By.CSS_SELECTOR, "#boxAddress > .opt-popup-title").text == "Para onde deseja enviar?"
+
+@when(u'preencho com o cep {cep}, clico no endereço e em aplicar')
+def step_impl(context, cep):
+    context.driver.find_element(By.ID, "inputSearchAddress").send_keys(cep)
+    context.driver.find_element(By.ID, "listAddressItems").click()
+    context.driver.find_element(By.CSS_SELECTOR, ".apply-button").click() 
+
+@when(u'clico na primeira promoção do banner')
+def step_impl(context):
+    context.driver.find_element(By.CSS_SELECTOR, "span[tabindex='0']").click()
+    context.driver.find_element(By.ID, "trBanner").click()
+
+@then(u'sou direcionado para página do {promocao_banner}')
+def step_impl(context, promocao_banner):
+    assert context.driver.find_element(By.ID, "title-defaut").text == promocao_banner
+
+@when(u'clico no produto "{nome_produto}" do id "{id_produto}"')
+def step_impl(context, nome_produto, id_produto):
+    assert context.driver.find_element(By.CSS_SELECTOR, f"a[data-idproduct='{id_produto}'] h2.title-item").text == nome_produto
+    context.driver.find_element(By.CSS_SELECTOR, f"a[data-idproduct='{id_produto}'] .image-content").click()
+    
+@then(u'sou direcionado para página de detalhes do produto')
+def step_impl(context):
+    
+
+
+@then(u'verifico se o nome do produto é "Buquê de 42 Rosas Cor de Rosa" e o preço é "R$ 359,90"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then verifico se o nome do produto é "Buquê de 42 Rosas Cor de Rosa" e o preço é "R$ 359,90"')
+
+
+@then(u'verifico a quantidade do produto antes de adicionar ao carrinho')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then verifico a quantidade do produto antes de adicionar ao carrinho')
+
+
+@when(u'clico em ADICIONAR AO CARRINHO')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When clico em ADICIONAR AO CARRINHO')
+
+
+@then(u'verifico se sou direcionado para a interface de selecionar a data e o período de entrega')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then verifico se sou direcionado para a interface de selecionar a data e o período de entrega')
+
+
+@when(u'seleciono a data de entrega para 31/10/2024 e o período para Comercial')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When seleciono a data de entrega para 31/10/2024 e o período para Comercial')
+
+
+@when(u'clico em OK')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When clico em OK')
+
+
+@then(u'sou direcionado para o Meu Carrinho')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then sou direcionado para o Meu Carrinho')
+
+
+@then(u'verifico se é produto "Buquê de 42 Rosas Cor de Rosa" se o preço é "R$ 359,90"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then verifico se é produto "Buquê de 42 Rosas Cor de Rosa" se o preço é "R$ 359,90"')
+
+
+@then(u'verifico se a quantidade é 1, o valor do frete é "R$ 15,90" e o valor total é "R$ 375,80"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then verifico se a quantidade é 1, o valor do frete é "R$ 15,90" e o valor total é "R$ 375,80"')
+
+
+@then(u'sou direcionado para a página de Entrega 01')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then sou direcionado para a página de Entrega 01')
+
+
+@when(u'preencho os campos de entrega com Luciana Benedita Renata Lima, 81983159224, 864, Residencial, 81983159224')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When preencho os campos de entrega com Luciana Benedita Renata Lima, 81983159224, 864, Residencial, 81983159224')
+
+
+@when(u'clico em Não quero cartão')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When clico em Não quero cartão')
+
+
+@then(u'sou direcionado para a página de pagamento')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then sou direcionado para a página de pagamento')
+
+
+@when(u'escolho a opção de pagamento com PIX')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When escolho a opção de pagamento com PIX')
+
+
+@when(u'clico em CONCLUIR COMPRA')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When clico em CONCLUIR COMPRA')
+
+
+@then(u'sou direcionado para a página de confirmação de compra')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then sou direcionado para a página de confirmação de compra')
